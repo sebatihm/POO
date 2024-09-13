66c933567d43317db84fe6779f872cdf51d005fb
Sebati HM <sebashermon003@gmail.com>
2024-09-13 15:19:02 -0600

    Menus

--- src/ui/UIMaestro.java
+++ src/ui/UIMaestro.java
@@ -1,7 +1,35 @@
 package ui;
 
+import java.util.Scanner;
+
+import static ui.UIMenu.*;
+
 public class UIMaestro {
     public static void maestroMenu(){
+        int respuesta;
+       do{
+           System.out.println("..:: Maestro ::..");
+           System.out.println("Seleccione una opcion: ");
+           System.out.println("1. Agregar Curso.");
+           System.out.println("2. Listar Curso.");
+           System.out.println("3. Salir");
 
+           Scanner sc = new Scanner(System.in);
+           respuesta = sc.nextInt();
+           switch (respuesta){
+               case 1:
+                   System.out.println("Agregar un curso:");
+                   break;
+               case 2:
+                   System.out.println("Listar un curso:");
+                   break;
+               case 3:
+                   mostrarMenu();
+                   break;
+               default:
+                   System.out.println("Seleccione una opcion valida");
+                   break;
+           }
+       }while(respuesta!=0);
     }
 }
--- src/ui/UIMenu.java
+++ src/ui/UIMenu.java
@@ -22,7 +22,8 @@ public class UIMenu {
             System.out.println("..:: Bienvenido ::..");
             System.out.println("Selecciona una opcion:");
             System.out.println("1. Maestros");
-            System.out.println("2. model.Estudiante");
+            System.out.println("2. Estudiante");
+            System.out.println("3. Secretaria");
             System.out.println("0. Salir");
 
 
@@ -32,13 +33,13 @@ public class UIMenu {
 
             switch(respuesta){
                 case 1:
-                    maestroMenu();
+                    loginUsuario(1);
                     break;
                 case 2:
-                    estudianteMenu();
+                    loginUsuario(2);
                     break;
                 case 3:
-                    secretariaMenu();
+                    loginUsuario(3);
                     break;
                 case 0:
                     System.out.println("Bye.");
@@ -50,6 +51,26 @@ public class UIMenu {
 
             }
         }while (respuesta != 0);
+    }
+
+    public static void loginUsuario(int tipoUsuario){
+        // Maestro 1
+        // Estudiante 2
+        // Secretaria 3
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa tu correo: ");
+        String correoUsuario = sc.nextLine();
+
+        if(tipoUsuario == 1){
+            for(Maestro maestro : maestros){
+                if(maestro.getCorreo().equals(correoUsuario)){
+                    // Maestro encontrado
+                    System.out.println("Hola maestro" + maestro.getNombre());
+                    maestroMenu();
+
+                }
+            }
+        }
     }
 
 
