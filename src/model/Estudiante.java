90c98df9b2c85b0c8e7370fd8f7e75d48f6090a0
Sebati HM <sebashermon003@gmail.com>
2024-09-25 18:19:27 -0600

    idk

--- src/model/Estudiante.java
+++ src/model/Estudiante.java
@@ -1,8 +1,12 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Estudiante extends Usuario{
     public String matricula;
     public String semestre;
+    ArrayList<CitaMaestro> tutoriasAgendadas = new ArrayList();
 
     public Estudiante(String nombre, String correo, String matricula, String semestre) {
         super(nombre, correo);
@@ -14,7 +18,11 @@ public class Estudiante extends Usuario{
         super(nombre);
 
     }
-
+    public void addTutoriaMaestro(Maestro maestro, Date fecha, String hora) {
+        CitaMaestro citaMaestro = new CitaMaestro(maestro, this);
+        citaMaestro.agendar(fecha,hora);
+        tutoriasAgendadas.add(new CitaMaestro(maestro, this));
+    }
     @Override
     public String toString() {
         return super.toString() + ", Matricula: " + matricula ;
